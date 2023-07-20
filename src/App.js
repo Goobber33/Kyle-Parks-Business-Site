60ac2354a846794a5bf079abf1c32ae069e6e90e
Kyle <kcparks1234@gmail.com>
2023-07-20 13:46:08 -0700

    Changed from css to sass

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import HomePage from './components/HomePage';
-import './App.css';
+import './styles/App.sass';
 
 function App() {
   return (
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 import AboutMe from './AboutMe';
 
 function HomePage() {
@@ -7,12 +7,12 @@ function HomePage() {
     <Container fluid className="px-0">
       {/* Hero Section */}
       <Row className="hero-section m-0">
-        <Col>
+        <div className="hero-text">
           <h1>Hello, I'm Kyle Parks</h1>
           <h2>Full Stack Developer</h2>
           <p>Welcome to my personal freelance website</p>
-          <Button variant="primary">Hire me</Button>
-        </Col>
+        </div>
+        <div className="hero-image"></div>
       </Row>
 
       <AboutMe />
