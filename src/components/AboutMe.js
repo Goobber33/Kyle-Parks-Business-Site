cd9bd34923cca24064523e9599099e99c53dbfb5
Kyle <kcparks1234@gmail.com>
2023-07-20 22:04:08 -0700

    Added a background to my about section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Col } from 'react-bootstrap';
+import { Col, Card } from 'react-bootstrap';
 
 function AboutMe() {
   return (
-    <div className="about-me">
-      
-        <Col>
-          <h2>About Me</h2>
-          <p>
-            I'm a freelance full stack developer with over 5 years of experience.
-            I specialize in React, Node.js, and MongoDB.
-          </p>
+    <div className="about-me d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+        <Col xs={12} md={6} lg={4}>
+            <Card bg="light" className="mb-4" style={{ minHeight: '300px' }}>
+                <Card.Body>
+                    <Card.Title>About Me</Card.Title>
+                    <Card.Text>
+                        I'm a freelance full stack developer with over 5 years of experience.
+                        I specialize in React, Node.js, and MongoDB.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
         </Col>
-      
     </div>
   );
 }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
+import { motion } from 'framer-motion';
+
+const textVariants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1, transition: { duration: 1 } }
+};
 
 function HomePage() {
   return (
@@ -7,13 +13,32 @@ function HomePage() {
       {/* Hero Section */}
       <Row className="hero-section m-0">
         <div className="hero-text">
-          <h1>Hello, I'm Kyle Parks</h1>
-          <h2>Full Stack Developer</h2>
-          <p>Welcome to my personal freelance website</p>
+          <motion.h1
+            variants={textVariants}
+            initial="hidden"
+            animate="visible"
+          >
+            Hello, My name is Kyle Parks
+          </motion.h1>
+          <motion.h2
+            variants={textVariants}
+            initial="hidden"
+            animate="visible"
+          >
+            Full Stack Developer
+          </motion.h2>
+          <motion.p
+            variants={textVariants}
+            initial="hidden"
+            animate="visible"
+          >
+           Specializing in building comprehensive, dynamic websites and applications, my expertise lies in an array of technologies that facilitate powerful, responsive, and user-friendly solutions.
+          From conceptualizing intuitive front-end interfaces to crafting robust back-end databases, my comprehensive approach ensures that every project I undertake is meticulously designed, rigorously tested, and effectively implemented.
+          Please explore further for a deeper understanding of my offerings, along with a showcase of my previous work, encapsulating the breadth and depth of my capabilities.
+          </motion.p>
         </div>
         <div className="hero-image"></div>
       </Row>
-
     </Container>
   );
 }
