0e9f98bb25b85ff6d87ddff8d7ad5b2e1429dd8c
Kyle <kcparks1234@gmail.com>
2023-07-24 16:23:53 -0700

    Made some changes to my hero image

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -2,21 +2,22 @@ import React from 'react';
 import { Col, Card } from 'react-bootstrap';
 
 function AboutMe() {
-  return (
-    <div className="about-me d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
-        <Col xs={12} md={6} lg={4}>
-            <Card bg="light" className="mb-4" style={{ minHeight: '300px' }}>
-                <Card.Body>
-                    <Card.Title>About Me</Card.Title>
-                    <Card.Text>
-                        I'm a freelance full stack developer with over 5 years of experience.
-                        I specialize in React, Node.js, and MongoDB.
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-        </Col>
-    </div>
-  );
+    return (
+        <div className="about-me d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+            <Col xs={12} md={6} lg={4}>
+                <Card bg="light" className="mb-4" style={{ minHeight: '300px' }}>
+                    <Card.Body>
+                        <Card.Title>About Me</Card.Title>
+                        <Card.Text>
+                            Specializing in building comprehensive, dynamic websites and applications, my expertise lies in an array of technologies that facilitate powerful, responsive, and user-friendly solutions.
+                            From conceptualizing intuitive front-end interfaces to crafting robust back-end databases, my comprehensive approach ensures that every project I undertake is meticulously designed, rigorously tested, and effectively implemented.
+                            Please explore further for a deeper understanding of my offerings, along with a showcase of my previous work, encapsulating the breadth and depth of my capabilities.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </div>
+    );
 }
 
 export default AboutMe;
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -11,14 +11,14 @@ function HomePage() {
   return (
     <Container fluid className="px-0">
       {/* Hero Section */}
-      <Row className="hero-section m-0">
+      <Row className="hero-section m-0 d-flex">
         <div className="hero-text">
           <motion.h1
             variants={textVariants}
             initial="hidden"
             animate="visible"
           >
-            Hello, My name is Kyle Parks
+            Kyle Parks
           </motion.h1>
           <motion.h2
             variants={textVariants}
@@ -27,17 +27,14 @@ function HomePage() {
           >
             Full Stack Developer
           </motion.h2>
-          <motion.p
-            variants={textVariants}
-            initial="hidden"
-            animate="visible"
-          >
-           Specializing in building comprehensive, dynamic websites and applications, my expertise lies in an array of technologies that facilitate powerful, responsive, and user-friendly solutions.
-          From conceptualizing intuitive front-end interfaces to crafting robust back-end databases, my comprehensive approach ensures that every project I undertake is meticulously designed, rigorously tested, and effectively implemented.
-          Please explore further for a deeper understanding of my offerings, along with a showcase of my previous work, encapsulating the breadth and depth of my capabilities.
-          </motion.p>
         </div>
-        <div className="hero-image"></div>
+        <div className="d-flex">
+          <div className="hero-image"></div>
+          <div className="hero-image"></div>
+          <div className="hero-image"></div>
+          <div className="hero-image"></div>
+          <div className="hero-image"></div>
+        </div>
       </Row>
     </Container>
   );
