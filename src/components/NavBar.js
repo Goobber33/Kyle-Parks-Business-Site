2e6b0882920a0d1fe7294ce27e0266f750a8358a
Kyle <kcparks1234@gmail.com>
2023-07-26 12:38:07 -0700

    Added styling to my home section and added media queries for mobile responsiveness

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,9 +5,9 @@ import '../styles/Navbar.css'
 function NavigationBar() {
   return (
     <Navbar variant="dark" expand="lg">
-      <Navbar.Brand href="#home">Kyle Parks</Navbar.Brand>
+      <Navbar.Brand href="#home"></Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
+      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
         <Nav className="ml-auto">
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#about">About</Nav.Link>
