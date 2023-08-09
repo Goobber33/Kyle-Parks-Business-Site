df4acc6dab8a61bf3ded64ac5fc6a457362a6d15
Kyle <kcparks1234@gmail.com>
2023-08-09 00:11:42 -0700

    Added media queries

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import '../styles/Home.scss'
+import '../styles/HomeMediaQueries.scss'
 
 const content = {
   heading1: "Kyle Parks",
@@ -60,41 +61,41 @@ function HomePage() {
   }, []);
 
   return (
-    <div style={{zIndex: '9998', position: 'relative'}}>
-    <Container fluid className="px-0">
-      <Row className="hero-section m-0 d-flex">
-        <div className="hero-text">
-          <h1 className={showText.heading1 ? "text-appear" : ""}>{content.heading1}</h1>
-          <h2 className={showText.heading2 ? "text-appear" : ""}>{content.heading2}</h2>
-          <div className="paragraph-box">
-            <p>{text.para}</p>
-          </div>
-  
-            <div className="script">
-                <lottie-player src="https://lottie.host/ff533659-9bc3-441b-8126-acb3446b42c7/KMemBJ1wH1.json" background="transparent" speed="1" style={{ width: 300, height: 300 }} loop autoplay></lottie-player>
+    <div style={{ zIndex: '9998', position: 'relative' }}>
+      <Container fluid className="px-0">
+        <Row className="hero-section m-0 d-flex">
+          <div className="hero-text">
+            <h1 className={showText.heading1 ? "text-appear" : ""}>{content.heading1}</h1>
+            <h2 className={showText.heading2 ? "text-appear" : ""}>{content.heading2}</h2>
+            <div className="paragraph-box">
+              <p>{text.para}</p>
             </div>
-        </div>
 
-        <div className="d-flex">
-          <div className="text-1">Food Trucks</div>
-          <div className="hero-image hero-image-1">
-            <div className="hero-image-caption">Food trucks are awesome! They bring local and world foods to our doorstep. Plus, you get to chat with the owners. They are some of my favorite clients!</div>
+            <div className="script">
+              <lottie-player className="lottie-animation" src="https://lottie.host/ff533659-9bc3-441b-8126-acb3446b42c7/KMemBJ1wH1.json" background="transparent" speed="1" style={{ width: 300, height: 300 }} loop autoplay></lottie-player>
+            </div>
           </div>
-          <div className="text-2">Gyms</div>
-          <div className="hero-image hero-image-2">
-            <div className="hero-image-caption">Gyms are the embodiment of strength and community. I take pride in providing them with custom digital solutions, creating seamless booking systems and interactive online platforms. Seeing these physical spaces extend their reach online is truly gratifying!</div>
+
+          <div className="d-flex">
+            <div className="text-1">Food Trucks</div>
+            <div className="hero-image hero-image-1">
+              <div className="hero-image-caption">Food trucks are awesome! They bring local and world foods to our doorstep. Plus, you get to chat with the owners. They are some of my favorite clients!</div>
+            </div>
+            <div className="text-2">Gyms</div>
+            <div className="hero-image hero-image-2">
+              <div className="hero-image-caption">Gyms are the embodiment of strength and community. I take pride in providing them with custom digital solutions, creating seamless booking systems and interactive online platforms. Seeing these physical spaces extend their reach online is truly gratifying!</div>
+            </div>
+            <div className="text-3">Restaurants</div>
+            <div className="hero-image hero-image-3">
+              <div className="hero-image-caption">For restaurants, every detail counts. I enjoy amplifying their culinary passion online with tailored digital experiences, including interactive menus and easy online ordering. Helping these establishments enhance their digital presence is always a delight!</div>
+            </div>
+            <div className="text-4">Portfolios</div>
+            <div className="hero-image hero-image-4">
+              <div className="hero-image-caption">A personal portfolio is a powerful showcase. Whether it's a visual artist or a professional writer, I love crafting bespoke digital galleries to highlight their work. It's rewarding to build these unique windows to my clients' worlds!</div>
+            </div>
           </div>
-          <div className="text-3">Restaurants</div>
-          <div className="hero-image hero-image-3">
-            <div className="hero-image-caption">For restaurants, every detail counts. I enjoy amplifying their culinary passion online with tailored digital experiences, including interactive menus and easy online ordering. Helping these establishments enhance their digital presence is always a delight!</div>
-          </div>
-          <div className="text-4">Portfolios</div>
-          <div className="hero-image hero-image-4">
-            <div className="hero-image-caption">A personal portfolio is a powerful showcase. Whether it's a visual artist or a professional writer, I love crafting bespoke digital galleries to highlight their work. It's rewarding to build these unique windows to my clients' worlds!</div>
-          </div>
-        </div>
-      </Row>
-    </Container>
+        </Row>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,17 +3,20 @@ import { Navbar, Nav } from 'react-bootstrap';
 import Headroom from 'react-headroom';
 import { Link } from 'react-scroll';
 import '../styles/Navbar.scss';
+import '../styles/NavQuery.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function NavigationBar() {
   return (
-    <div style={{zIndex: '9999', position: 'relative'}}>
-      <Headroom>
+    <div style={{ zIndex: '9999', position: 'relative', width: '100%' }}>
+      <Headroom style={{ position: "fixed", width: "100%", top: "0", zIndex: "0" }}>
         <div className="navbar-wrapper">
           <Navbar variant="dark" expand="lg">
             <Navbar.Brand href="#home"></Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            {/* The Navbar.Toggle is now outside the Navbar.Collapse to ensure it remains centered */}
+            <Navbar.Toggle aria-controls="basic-navbar-nav" className="centered-toggle" />
             <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
-              <Nav className="ml-auto">
+              <Nav className="m-auto">
                 <Link 
                   activeClass="active"
                   to="home"
