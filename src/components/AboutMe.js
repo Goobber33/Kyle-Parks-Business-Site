5f5571d540d8490979b546cf843a7f9b1bf091a9
Kyle <kcparks1234@gmail.com>
2023-08-14 23:08:47 -0700

    Changed styling for my card

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { Button, Col, Row } from 'react-bootstrap';
 import { useInView } from 'react-intersection-observer';
 import ScrollIntoViewIfNeeded from 'react-scroll-into-view-if-needed';
+import { faGlobe, faMobileAlt, faUserCheck, faChartLine } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../styles/About.scss';
 import '../styles/AboutQueries.scss';
-import kyleImage from '../assets/images/kyle.jpeg';
 
 function AboutMe() {
     const [shouldScroll, setShouldScroll] = useState(false);
@@ -37,23 +38,31 @@ function AboutMe() {
                         <div className="flip-card">
                             <div className={`flip-card-inner ${isFlipped ? "flip" : ""}`}>
                                 <div className="flip-card-front">
-                                    <div className="image-and-title">
-                                        <img className="my-image" src={kyleImage} alt="Kyle" />
-                                        <h1 className='h1-text'>Founder <span style={{ color: '#2EC4B6' }}>/</span> Owner</h1>
+                                    <div className="header-container">
+                                        <h1>Your Business Online</h1>
+                                        <h2>Why Local Businesses Need a Strong Digital Presence</h2>
                                     </div>
+
                                     <div className="front-text">
-                                        <p>Hello! My name is Kyle Parks, I am the founder and owner of this business.</p>
-                                        <Button className="my-button" onClick={handleClick} >Click here to learn more about me!</Button>
+                                        <div className="statistics">
+                                            <div className="stat-item">
+                                                <FontAwesomeIcon icon={faGlobe} /><p>97% of consumers search online for local services.</p>
+                                            </div>
+                                            <div className="stat-item">
+                                                <FontAwesomeIcon icon={faMobileAlt} /><p>52% of all online traffic comes from mobile devices.</p>
+                                            </div>
+                                            <div className="stat-item">
+                                                <FontAwesomeIcon icon={faUserCheck} /><p>75% of consumers judge company credibility by website design.</p>
+                                            </div>
+                                            <div className="stat-item">
+                                                <FontAwesomeIcon icon={faChartLine} /><p>Small businesses with websites grow 15.7% faster.</p>
+                                            </div>
+                                        </div>
+                                        <Button className="my-button" onClick={handleClick}>Next</Button>
                                     </div>
                                 </div>
                                 <div className="flip-card-back">
-                                    <p>
-                                        Greetings! I'm a dedicated Full Stack Developer with a passion for crafting innovative digital solutions.
-                                        I specialize in building everything from sophisticated websites to dynamic mobile applications, and intricate databases.
-                                        As the founder and sole developer of this business, my commitment is unmatched.
-                                        Explore my offerings and get in touch—I'm excited to turn your dream website into a reality.
-                                    </p>
-                                    <Button className="my-button" onClick={handleClick} >Back</Button>
+                                    <Button className="my-button" onClick={handleClick}>Back</Button>
                                 </div>
                             </div>
                         </div>
